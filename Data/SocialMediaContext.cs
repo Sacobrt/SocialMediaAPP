ba907560dbfdb65a685f622179f971e21287195e
Sacobrt <sacerdanijel@gmail.com>
2024-08-23 15:47:19 +0200

    data SocialMediaContext

---
+++ Data/SocialMediaContext.cs
@@ -0,0 +1,13 @@
+﻿using CSHARP_SocialMediaAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSHARP_SocialMediaAPP.Data
+{
+    public class SocialMediaContext : DbContext
+    {
+        public SocialMediaContext(DbContextOptions<SocialMediaContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
