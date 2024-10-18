8352a779dd709a3268060f57e8b0f6064da9bd31
Sacobrt <sacerdanijel@gmail.com>
2024-10-18 05:58:58 +0200

    added border on top footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 text-gray-400 py-6">
+        <footer className="bg-gray-900 text-gray-400 py-6 border-t-2 border-gray-700">
             <div className="container mx-auto px-4 flex flex-col items-center">
                 {/* Logo Section */}
                 <div className="mb-4">
