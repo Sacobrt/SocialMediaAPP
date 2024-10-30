658016e18995829affffba364fb165e04176ffa1
Sacobrt <sacerdanijel@gmail.com>
2024-10-30 09:36:28 +0100

    changed text size for smaller screens

--- Frontend/src/components/TotalData.jsx
+++ Frontend/src/components/TotalData.jsx
@@ -24,21 +24,21 @@ function TotalData() {
             <div className="shadow-lg hover:shadow-xl transition-all duration-500 ease-in-out rounded-lg p-8 w-full max-w-4xl text-center">
                 <div className="grid grid-cols-3 gap-8">
                     <div className="flex flex-col items-center">
-                        <p className="text-4xl font-extrabold text-gray-300">
+                        <p className="text-2xl md:text-4xl font-extrabold text-gray-300">
                             <CountUp start={0} end={totalData.users} duration={20} separator="."></CountUp>
                         </p>
                         <p className="text-sm text-gray-500">Active Members</p>
                     </div>
 
                     <div className="flex flex-col items-center">
-                        <p className="text-4xl font-extrabold text-gray-300">
+                        <p className="text-2xl md:text-4xl font-extrabold text-gray-300">
                             <CountUp start={0} end={totalData.posts} duration={20} separator="."></CountUp>
                         </p>
                         <p className="text-sm text-gray-500">Active Posts</p>
                     </div>
 
                     <div className="flex flex-col items-center">
-                        <p className="text-4xl font-extrabold text-gray-300">
+                        <p className="text-2xl md:text-4xl font-extrabold text-gray-300">
                             <CountUp start={0} end={totalData.comments} duration={20} separator="."></CountUp>
                         </p>
                         <p className="text-sm text-gray-500">Total Comments</p>
